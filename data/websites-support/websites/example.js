71eae88bb006d8f259ff85d32788e29fa8a42464
Jay <juaoose@users.noreply.github.com>
2015-09-06 02:26:04 -0500

    Create example.js

---
+++ data/websites-support/websites/example.js
@@ -0,0 +1,34 @@
+var ExampleTrackListener = function() { };
+ExampleTrackListener.prototype = new Common.WebsiteTrackListener();
+
+ExampleTrackListener.prototype.isPlaying = function() {
+	return true;
+};
+
+ExampleTrackListener.prototype.findSelector = function() {
+	this.selector = undefined;
+};
+
+ExampleTrackListener.prototype.scrapPlayData = function() {
+	this.artistName = "";
+	this.trackName  = "";
+	return true;
+};
+
+ExampleTrackListener.prototype.scrapAlbumName = function() {
+	return ""; 
+};
+
+ExampleTrackListener.prototype.scrapAlbumArt = function() {
+	return "";
+};
+
+ExampleTrackListener.prototype.scrapUrl = function() {
+	return "";
+};
+
+ExampleTrackListener.prototype.scrapDuration = function() {
+	return "";
+};
+
+Common.runTrackListenerInterval(new ExampleTrackListener());
