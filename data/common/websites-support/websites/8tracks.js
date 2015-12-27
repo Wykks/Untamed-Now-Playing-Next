f6a4f4e8f1ea46d60edf1ee66709be53e8780705
Wykks <wykks@users.noreply.github.com>
2015-12-27 10:24:54 -0800

    fix 8tracks album & url

--- data/common/websites-support/websites/8tracks.js
+++ data/common/websites-support/websites/8tracks.js
@@ -1,30 +1,30 @@
-var HeightTracksTrackListener = function() {};
+const HeightTracksTrackListener = function () {};
 HeightTracksTrackListener.prototype = new Common.WebsiteTrackListener();
 
-HeightTracksTrackListener.prototype.findSelector = function() {
-	this.selector = $('li.now_playing').find('.title_container').find('.title_artist');
+HeightTracksTrackListener.prototype.findSelector = function () {
+    this.selector = $('li.now_playing').find('.title_container').find('.title_artist');
 };
 
-HeightTracksTrackListener.prototype.isPlaying = function() {
-	return $('#player_pause_button').css('display') == 'block';
+HeightTracksTrackListener.prototype.isPlaying = function () {
+    return $('#player_pause_button').css('display') == 'block';
 };
 
-HeightTracksTrackListener.prototype.scrapPlayData = function() {
-	this.artistName = this.selector.find('.a').eq(0).text();
-	this.trackName  = this.selector.find('.t').eq(0).text();
-	return true;
+HeightTracksTrackListener.prototype.scrapPlayData = function () {
+    this.artistName = this.selector.find('.a').eq(0).text();
+    this.trackName = this.selector.find('.t').eq(0).text();
+    return true;
 };
 
-HeightTracksTrackListener.prototype.scrapAlbumName = function() {
-	return $('#now_playing').find('.album').find('.detail').text();
+HeightTracksTrackListener.prototype.scrapAlbumName = function () {
+    return $('#now_playing .album .detail').text();
 };
 
-HeightTracksTrackListener.prototype.scrapAlbumArt = function() {
-	return $('#player_mix > a > img').attr('src').replace(/(.*)\?(.*)/i, '$1');
+HeightTracksTrackListener.prototype.scrapAlbumArt = function () {
+    return $('#player_mix > a > img').attr('src').replace(/(.*)\?(.*)/i, '$1');
 };
 
-HeightTracksTrackListener.prototype.scrapUrl = function() {
-	return 'http://8tracks.com' + $('#player_mix > a').attr('href');
+HeightTracksTrackListener.prototype.scrapUrl = function () {
+    return 'http://8tracks.com' + $('#player #player_mix').children().attr('href');
 };
 
 Common.runTrackListenerInterval(new HeightTracksTrackListener());
