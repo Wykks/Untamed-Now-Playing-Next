3f739d43c08702b60e01c710a817678bb222f8b1
Wykks <wykks@users.noreply.github.com>
2016-01-29 21:16:47 +0100

    fix youtubetv (silly me again..)

--- data/common/websites-support/websites/youtubetv.js
+++ data/common/websites-support/websites/youtubetv.js
@@ -1,25 +1,30 @@
-if (BrowserFunc.getOption('unpDisableYoutube') === true)
-    throw new Error('UNP disabled on youtube');
+const youtubetv = (options) => {
+    if (options.unpDisableYoutube === true) {
+        throw new Error('UNP disabled on youtube');
+    }
 
-const YoutubeTVTrackListener = function() {};
-YoutubeTVTrackListener.prototype = new Common.WebsiteTrackListener();
+    const YoutubeTVTrackListener = function() {};
+    YoutubeTVTrackListener.prototype = new Common.WebsiteTrackListener();
 
-YoutubeTVTrackListener.prototype.isPlaying = function() {
-    return $('#watch').hasClass('play');
+    YoutubeTVTrackListener.prototype.isPlaying = function() {
+        return $('#watch').hasClass('play');
+    };
+
+    YoutubeTVTrackListener.prototype.scrapPlayData = function() {
+        const play = $('#title-tray .player-video-title').text();
+        [this.artistName, this.trackName] = Common.parseArtistTitle(play);
+        return true;
+    };
+
+    YoutubeTVTrackListener.prototype.scrapDuration = function() {
+        return $('#bottom-half .player-time-total').text();
+    };
+
+    const updateTriggerer = new Common.MutationObserverUpdater(new YoutubeTVTrackListener());
+    updateTriggerer.setSelector('#watch');
+    updateTriggerer.setNodeAttributeName('class');
+    updateTriggerer.setNodeAttributeValue(new RegExp('play'));
+    updateTriggerer.runOnAttr();
 };
 
-YoutubeTVTrackListener.prototype.scrapPlayData = function() {
-    const play = $('#title-tray .player-video-title').text();
-    [this.artistName, this.trackName] = Common.parseArtistTitle(play);
-    return true;
-};
-
-YoutubeTVTrackListener.prototype.scrapDuration = function() {
-    return $('#bottom-half .player-time-total').text();
-};
-
-const updateTriggerer = new Common.MutationObserverUpdater(new YoutubeTVTrackListener());
-updateTriggerer.setSelector('#watch');
-updateTriggerer.setNodeAttributeName('class');
-updateTriggerer.setNodeAttributeValue(new RegExp('play'));
-updateTriggerer.runOnAttr();
+BrowserFunc.getOptions().then(youtubetv);
