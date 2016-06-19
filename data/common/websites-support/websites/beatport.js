e2561afd95faa68c13cbb40bb0644975292d169c
Wykks <wykks@users.noreply.github.com>
2016-06-19 21:18:59 +0200

    fix beatport.com support

--- data/common/websites-support/websites/beatport.js
+++ data/common/websites-support/websites/beatport.js
@@ -2,37 +2,29 @@ const BeatportTrackListener = function() {};
 BeatportTrackListener.prototype = new window.UNPCommon.WebsiteTrackListener();
 
 BeatportTrackListener.prototype.isPlaying = function() {
-    return $('.omniplayer').hasClass('is-playing');
+    return $('.player-container.player-section .player-buttons-container .play-button').hasClass('pause');
 };
 
 BeatportTrackListener.prototype.findSelector = function() {
-    this.selector = $('.omniplayer');
+    this.selector = $('.player-current-track-container');
 };
 
 BeatportTrackListener.prototype.scrapPlayData = function() {
     this.artistName = '';
     const self = this;
-    this.selector.find('.omniplayer--artist > div > a').each(function() {
+    this.selector.find('.player-track-name-artist-standard > .track-artist').each(function() {
         self.artistName += (self.artistName === '') ? $.trim($(this).text()) : ', ' + $.trim($(this).text());
     });
-    this.trackName = $.trim(this.selector.find('a.omniplayer--title').contents().filter(
-        function() {
-            return this.nodeType == Node.TEXT_NODE;
-        }
-    ).text());
+    this.trackName = this.selector.find('.track-title .primary-title').text();
     return true;
 };
 
 BeatportTrackListener.prototype.scrapAlbumArt = function() {
-    return this.selector.find('img.audio-control--art').attr('src').replace('60x60', '');
+    return this.selector.find('.track-artwork').attr('src').replace('45x45', '300x300');
 };
 
 BeatportTrackListener.prototype.scrapUrl = function() {
-    return 'https://www.beatport.com' + this.selector.find('a.omniplayer--title').attr('href');
-};
-
-BeatportTrackListener.prototype.scrapDuration = function() {
-    return $.trim(this.selector.find('.omniplayer--extendable > .omniplayer--progress > .omniplayer--duration:eq(1)').text());
+    return this.selector.find('.track-artwork-link').attr('href');
 };
 
 window.UNPCommon.runTrackListenerInterval(new BeatportTrackListener());
