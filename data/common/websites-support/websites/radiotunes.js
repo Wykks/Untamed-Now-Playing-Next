3ef79306f7b5eacd4fcd4afcf5b83fc696d979d9
Kovalchuk Evgeny <kovalchuk.evgeny@gmail.com>
2016-08-20 09:56:54 +0300

    Fix radiotunes.js

--- data/common/websites-support/websites/radiotunes.js
+++ data/common/websites-support/websites/radiotunes.js
@@ -2,20 +2,20 @@ const RadiotunesTrackListener = function() {};
 RadiotunesTrackListener.prototype = new window.UNPCommon.WebsiteTrackListener();
 
 RadiotunesTrackListener.prototype.isPlaying = function() {
-    return $('#ctl-play').find('.icon').hasClass('icon-stop');
+	return $('#play-button').find('.icon').hasClass('icon-stop');
 };
 
 RadiotunesTrackListener.prototype.scrapPlayData = function() {
-    const play = $('.title-container.title').text();
-    [this.artistName, this.trackName] = window.UNPCommon.parseArtistTitle(play);
-    return true;
+	const play = $('#now-playing').find('.title').text();
+	[this.artistName, this.trackName] = window.UNPCommon.parseArtistTitle(play);
+	return true;
 };
 
 RadiotunesTrackListener.prototype.scrapAlbumArt = function() {
-        const link = $('#art').find('img').attr('src');
-        if (link.substring(0, 4) != 'http')
-            return 'https:' + link;
-        return link;
-    };
+	const link = $('#art').find('img').first().attr('src');
+	if (link.substring(0, 4) != 'http')
+		return 'https:' + link;
+	return link;
+};
 
 window.UNPCommon.runTrackListenerInterval(new RadiotunesTrackListener());
