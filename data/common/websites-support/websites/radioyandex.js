bb57f05a982fc54cf7ef5ec5557cee23bad0c465
ten0r <ten0r@yandex.ru>
2016-09-24 20:39:44 +0300

    Moved from page parsing to js variables
    Is it ok to use unsafeWindow?
    By the wat all features have been implemented.

--- data/common/websites-support/websites/radioyandex.js
+++ data/common/websites-support/websites/radioyandex.js
@@ -1,22 +1,38 @@
 const RadioYandexTrackListener = function() {};
 RadioYandexTrackListener.prototype = new window.UNPCommon.WebsiteTrackListener();
 
-RadioYandexTrackListener.prototype.isPlaying = function() {
-    return $('body').hasClass('body_state_playing');
+RadioYandexTrackListener.prototype.isPlaying = function () {
+    this.music = unsafeWindow.Mu;
+    return (this.music.Flow.getState() === 'playing');
 };
 
-RadioYandexTrackListener.prototype.findSelector = function() {
-    this.selector = $('.slider__item_playing');
-};
-
-RadioYandexTrackListener.prototype.scrapPlayData = function() {
-    this.artistName = this.selector.find('.track__artists').attr('title');
-    this.trackName = this.selector.find('.track__title').attr('title');
+RadioYandexTrackListener.prototype.scrapPlayData = function () {
+    this.artistName = this.music.Flow.getCurrent().artists[0].name;
+    this.trackName = this.music.Flow.getCurrent().title;
     return true;
 };
 
-RadioYandexTrackListener.prototype.scrapAlbumArt = function() {
-    return this.selector.find(".track__cover").css('background-image').replace('url("', '').replace('")', '');
+RadioYandexTrackListener.prototype.scrapAlbumArt = function () {
+    return 'https://'+this.music.Flow.getCurrent().albums[0].coverUri.replace('%%', '600x600');
+};
+
+RadioYandexTrackListener.prototype.scrapDuration = function () {
+    var time = this.music.Flow.getDuration().toFixed(0);
+    var hours = parseInt(time / 3600) % 24;
+    var minutes = parseInt(time / 60) % 60;
+    var seconds = time % 60;
+    return (hours > 0 ? hours+':' : '') +
+        + ((hours > 0 && minutes < 10) ? '0' + minutes : minutes) + ':'
+        + ((minutes > 0 && seconds < 10) ? '0' + seconds : seconds);
+};
+
+RadioYandexTrackListener.prototype.scrapAlbumName = function () {
+    return this.music.Flow.getCurrent().albums[0].title;
+};
+
+RadioYandexTrackListener.prototype.scrapUrl = function () {
+    return link = 'https://music.yandex.ru/album/' + this.music.Flow.getCurrent().albums[0].id
+        + "/track/" + this.music.Flow.getCurrent().id;
 };
 
 window.UNPCommon.runTrackListenerInterval(new RadioYandexTrackListener());
