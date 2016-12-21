c97cc018d618a3c482af68c018c0ede73dde4b55
Wykks <wykks@users.noreply.github.com>
2016-12-21 22:12:59 +0100

    update libs and add a very basic script

---
+++ data/third-party/updateDeps.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+set -e
+
+wget https://unpkg.com/preact-markup@latest/dist/preact-markup.min.js -O preact-markup.min.js
+wget https://unpkg.com/preact@latest/dist/preact.min.js -O preact.min.js
+wget https://unpkg.com/preact-router@latest/dist/preact-router.min.js -O preact-router.min.js
+wget https://unpkg.com/history@latest/umd/history.min.js -O history.min.js
