1b97f43111063877cf6b506dd0ba415d2fdec305
Wykks <wykks@users.noreply.github.com>
2017-02-25 17:32:53 +0100

    update dependencies

--- data/third-party/updateDeps.sh
+++ data/third-party/updateDeps.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget https://unpkg.com/preact-markup@latest/dist/preact-markup.min.js -O preact-markup.min.js
-wget https://unpkg.com/preact@latest/dist/preact.min.js -O preact.min.js
-wget https://unpkg.com/preact-router@latest/dist/preact-router.min.js -O preact-router.min.js
-wget https://unpkg.com/history@latest/umd/history.min.js -O history.min.js
+wget https://unpkg.com/preact-markup@1.6.0/dist/preact-markup.min.js -O preact-markup.min.js
+wget https://unpkg.com/preact@7.2.0/dist/preact.min.js -O preact.min.js
+wget https://unpkg.com/preact-router@2.4.1/dist/preact-router.min.js -O preact-router.min.js
+wget https://unpkg.com/history@4.5.1/umd/history.min.js -O history.min.js
