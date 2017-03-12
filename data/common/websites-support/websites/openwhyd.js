9ceca8cc38996e979f585d2221486eb655a3bc36
Wykks <wykks@users.noreply.github.com>
2017-03-12 16:51:10 +0100

    adjust a bit openwhyd file

--- data/common/websites-support/websites/openwhyd.js
+++ data/common/websites-support/websites/openwhyd.js
@@ -1,23 +1,17 @@
-const OpenwhydTrackListener = function() {};
+const OpenwhydTrackListener = function () { };
 OpenwhydTrackListener.prototype = new window.UNPCommon.WebsiteTrackListener();
 
-OpenwhydTrackListener.prototype.isPlaying = function() {
-   return true;
-};
-
-OpenwhydTrackListener.prototype.findSelector = function() {
-    this.selector = undefined;
-};
-
-OpenwhydTrackListener.prototype.scrapPlayData = function() {
-	if ($('#trackTitle').find('a').length) {
-		[this.artistName, this.trackName] = window.UNPCommon.parseArtistTitle($('#trackTitle').find('a').text());
-	}
-    else {
-		this.artistName = '';
-		this.trackName = '';
-	}
+OpenwhydTrackListener.prototype.isPlaying = function () {
     return true;
 };
 
-window.UNPCommon.runTrackListenerInterval(new OpenwhydTrackListener())
+OpenwhydTrackListener.prototype.scrapPlayData = function () {
+    if ($('#trackTitle').find('a').length) {
+        [this.artistName, this.trackName] = window.UNPCommon.parseArtistTitle($('#trackTitle').find('a').text());
+    } else {
+        return false;
+    }
+    return true;
+};
+
+window.UNPCommon.runTrackListenerInterval(new OpenwhydTrackListener());
