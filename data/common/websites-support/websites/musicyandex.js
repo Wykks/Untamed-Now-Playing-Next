a1dbd84349d1fa9e5e54692cf766dcc50dafab31
Kovalchuk Evgeny <kovalchuk.evgeny@gmail.com>
2017-06-11 10:01:54 +0300

    fix Yandex Music

--- data/common/websites-support/websites/musicyandex.js
+++ data/common/websites-support/websites/musicyandex.js
@@ -16,7 +16,7 @@ MusicYandexTrackListener.prototype.scrapPlayData = function() {
 };
 
 MusicYandexTrackListener.prototype.scrapAlbumArt = function() {
-    const link = this.selector.find('img.album-cover').attr('src');
+    const link = this.selector.find('img.track-cover').attr('src').replace('50x50', '400x400');
     if (link.substring(0, 4) != 'http')
         return 'https:' + link;
     return link;
@@ -27,7 +27,7 @@ MusicYandexTrackListener.prototype.scrapUrl = function() {
 };
 
 MusicYandexTrackListener.prototype.scrapDuration = function() {
-    return $('.progress > .progress__progress > .progress__right').text();
+    return $('.progress').find('.progress__text').find('.progress__right').text();
 };
 
 window.UNPCommon.runTrackListenerInterval(new MusicYandexTrackListener());
