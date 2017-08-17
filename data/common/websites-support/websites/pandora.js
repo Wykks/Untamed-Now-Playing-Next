12d129e2267a933045643344831f46026bde9cb5
Senpos <kovalchuk.evgeny@gmail.com>
2017-08-17 10:19:24 +0300

    Change a selector from where Pandora grabs data

--- data/common/websites-support/websites/pandora.js
+++ data/common/websites-support/websites/pandora.js
@@ -6,14 +6,14 @@ PandoraTrackListener.prototype.isPlaying = function() {
 };
 
 PandoraTrackListener.prototype.findSelector = function() {
-    this.selector = $('div.NowPlaying__aboveFold');
+    this.selector = $('div.Tuner__ContentWrapper');
 };
 
 PandoraTrackListener.prototype.scrapPlayData = function() {
-    this.artistName = this.selector.find('a[data-qa=playing_artist_name]').text();
+    this.artistName = this.selector.find('div.Tuner__Audio__TrackDetail__artist > a[data-qa=mini_track_artist_name]').text();
 
     // since track name and url are in the same selector, doing all stuff here
-    this.trackSelector = this.selector.find('a[data-qa=playing_track_title]');
+    this.trackSelector = this.selector.find('.Tuner__Audio__TrackDetail__title > a[data-qa=mini_track_title]');
     this.trackName = this.trackSelector.text();
     this.url = 'https://pandora.com' + this.trackSelector.attr('href');
     return true;
@@ -24,7 +24,7 @@ PandoraTrackListener.prototype.scrapAlbumName = function() {
 };
 
 PandoraTrackListener.prototype.scrapAlbumArt = function() {
-    return this.selector.find('div[data-qa=album_active_image]').css('background-image').replace('url("', '').replace('")', '');
+    return this.selector.find('div.Tuner__Audio__TrackDetail__img').find('img').attr('src').replace('90W_90H', '500W_500H');
 };
 
 PandoraTrackListener.prototype.scrapDuration = function() {
