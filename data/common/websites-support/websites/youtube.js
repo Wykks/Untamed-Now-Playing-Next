c7157696494379f29aa78b97cc5d3078756d2e29
Senpos <kovalchuk.evgeny@gmail.com>
2017-10-09 10:55:19 +0300

    Fix YT thumbnails scraping

--- data/common/websites-support/websites/youtube.js
+++ data/common/websites-support/websites/youtube.js
@@ -1,3 +1,11 @@
+// helpers
+const getVideoId = function(video_url) {
+    var match = video_url.match(/v=([\d\w]+)/);
+    if (match) {
+        return match[1]; // matching group 1 with ID
+    }
+}
+
 const youtube = (options) => {
     if (options.unpDisableYoutube === true) {
         throw new Error('UNP disabled on youtube');
@@ -8,7 +16,7 @@ const youtube = (options) => {
     YoutubeTrackListener.prototype.isPlaying = function () {
         return $('#movie_player').hasClass('playing-mode');
     };
-    
+
     YoutubeTrackListener.prototype.scrapPlayData = function () {
         const play = $('#player-container').find('.ytp-title-text').text();
         [this.artistName, this.trackName] = window.UNPCommon.parseArtistTitle(play);
@@ -17,15 +25,20 @@ const youtube = (options) => {
         }
         return true;
     };
-    
-    YoutubeTrackListener.prototype.scrapAlbumArt = function () {
-        return $('#thumbnail').find('#img').attr('src');
-    };
-    
+
     YoutubeTrackListener.prototype.scrapUrl = function () {
         return $('.ytp-title-link ').attr('href');
     };
-    
+
+    YoutubeTrackListener.prototype.scrapAlbumArt = function () {
+        // since i didn't find thumbnail as usual, it is easier to get
+        // video ID and substitute to the thumbnail's default url
+        var id = getVideoId(this.scrapUrl());
+        if (id) {
+            return 'https://i.ytimg.com/vi/' + id + '/hqdefault.jpg'
+        }
+    };
+
     YoutubeTrackListener.prototype.scrapDuration = function () {
         return $('.ytp-time-duration').text();
     };
