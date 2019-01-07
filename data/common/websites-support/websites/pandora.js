02d2ad0df9c4556781ca59b15982355199a5eb8c
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2019-01-07 22:11:46 +0200

    Revert changes to use miniplayer again, album name gone

--- data/common/websites-support/websites/pandora.js
+++ data/common/websites-support/websites/pandora.js
@@ -2,34 +2,29 @@ const PandoraTrackListener = function() {};
 PandoraTrackListener.prototype = new window.UNPCommon.WebsiteTrackListener();
 
 PandoraTrackListener.prototype.isPlaying = function() {
-    return $('button[data-qa=pause_button]').length ? true : false;
+    return !!$('button[data-qa=pause_button]').length;
 };
 
 PandoraTrackListener.prototype.findSelector = function() {
-    this.selector = $('div.nowPlayingTopInfo');
+    this.selector = $('div.Tuner__ContentWrapper');
 };
 
 PandoraTrackListener.prototype.scrapPlayData = function() {
-    this.artistName = this.selector.find('a.nowPlayingTopInfo__current__artistName').text();
+    this.artistName = this.selector.find('.Tuner__Audio__TrackDetail__artist[data-qa=mini_track_artist_name]').text();
 
     // since track name and url are in the same selector, doing all stuff here
-    this.trackSelector = this.selector.find('a.nowPlayingTopInfo__current__trackName');
+    this.trackSelector = this.selector.find('.Tuner__Audio__TrackDetail__title[data-qa=mini_track_title]');
     this.trackName = this.trackSelector.text();
     this.url = 'https://pandora.com' + this.trackSelector.attr('href');
     return true;
 };
 
-PandoraTrackListener.prototype.scrapAlbumName = function() {
-    return this.selector.find('a.nowPlayingTopInfo__current__albumName').text();
-};
-
 PandoraTrackListener.prototype.scrapAlbumArt = function() {
-    var albumArtSelector = this.selector.find('div.nowPlayingTopInfo__artContainer__art[data-qa=album_active_image]');
-    return albumArtSelector.length ? albumArtSelector.css('background-image').replace('url("', '').replace('")', '') : false;
+    return this.selector.find('.Tuner__Audio__TrackDetail__img').find('img').attr('src').replace('90W_90H', '500W_500H');
 };
 
 PandoraTrackListener.prototype.scrapDuration = function() {
-    return $('.Duration > span[data-qa=remaining_time]').text();
+    return $('span[data-qa=remaining_time]').text();
 };
 
 window.UNPCommon.runTrackListenerInterval(new PandoraTrackListener());
