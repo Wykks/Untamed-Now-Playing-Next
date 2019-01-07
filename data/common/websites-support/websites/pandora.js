df104b591c46acf36a84784edc937d24fbe56fad
Evgeny Kovalchuk <kovalchuk.evgeny@gmail.com>
2019-01-07 09:21:25 +0200

    Fix pandora.com

--- data/common/websites-support/websites/pandora.js
+++ data/common/websites-support/websites/pandora.js
@@ -6,29 +6,29 @@ PandoraTrackListener.prototype.isPlaying = function() {
 };
 
 PandoraTrackListener.prototype.findSelector = function() {
-    this.selector = $('div.Tuner__ContentWrapper');
+    this.selector = $('div.nowPlayingTopInfo');
 };
 
 PandoraTrackListener.prototype.scrapPlayData = function() {
-    this.artistName = this.selector.find('div.Tuner__Audio__TrackDetail__artist > a[data-qa=mini_track_artist_name]').text();
+    this.artistName = this.selector.find('a.nowPlayingTopInfo__current__artistName').text();
 
     // since track name and url are in the same selector, doing all stuff here
-    this.trackSelector = this.selector.find('.Tuner__Audio__TrackDetail__title > a[data-qa=mini_track_title]');
+    this.trackSelector = this.selector.find('a.nowPlayingTopInfo__current__trackName');
     this.trackName = this.trackSelector.text();
     this.url = 'https://pandora.com' + this.trackSelector.attr('href');
     return true;
 };
 
 PandoraTrackListener.prototype.scrapAlbumName = function() {
-    return this.selector.find('a[data-qa=playing_album_name]').text();
+    return this.selector.find('a.nowPlayingTopInfo__current__albumName').text();
 };
 
 PandoraTrackListener.prototype.scrapAlbumArt = function() {
-    return this.selector.find('div.Tuner__Audio__TrackDetail__img').find('img').attr('src').replace('90W_90H', '500W_500H');
+    return this.selector.find('div.nowPlayingTopInfo__artContainer__art[data-qa=album_active_image]').css('background-image').replace('url("', '').replace('")', '');
 };
 
 PandoraTrackListener.prototype.scrapDuration = function() {
-    return $('span[data-qa=remaining_time]').text();
+    return $('.Duration > span[data-qa=remaining_time]').text();
 };
 
 window.UNPCommon.runTrackListenerInterval(new PandoraTrackListener());
